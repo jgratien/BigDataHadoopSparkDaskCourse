083766ef7a1629340746aa73723aa4a5ed8505f2
ramyk <ramy.kader.rk@gmail.com>
2021-02-21 20:24:54 +0100

    unsupport cluster-mode for python

--- TPs/Project/submit.sh
+++ TPs/Project/submit.sh
@@ -1,5 +1,4 @@
 spark-submit \
---conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=$PYSPARK_PYTHON \
 --master spark://front-in1.cemef:7077 \
---deploy-mode cluster \
+--conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=$PYSPARK_PYTHON \
 $1
