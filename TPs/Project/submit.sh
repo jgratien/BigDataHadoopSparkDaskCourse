f0a9e33a1a092898b4202c8c51b3175e3d6492df
ramyk <ramy.kader.rk@gmail.com>
2021-02-21 20:52:44 +0100

    unset INFO logging

--- TPs/Project/submit.sh
+++ TPs/Project/submit.sh
@@ -1,5 +1,5 @@
 spark-submit \
 --master    spark://front-in1.cemef:7077 \
 --conf      spark.yarn.appMasterEnv.PYSPARK_PYTHON=$PYSPARK_PYTHON \
---conf      log4j.rootCategory=INFO \
+--conf      log4j.rootCategory=ERROR \
 $1
