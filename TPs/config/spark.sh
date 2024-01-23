46d0beb4fc48540dfe9e16d74a492a9c83479165
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2024-01-23 14:13:01 +0100

    update

---
+++ TPs/config/spark.sh
@@ -0,0 +1,5 @@
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export SPARK_HOME=/home/hduser/local/spark
+export PATH=$SPARK_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native:$LD_LIBRARY_PATH
+#setenv SPARK_LOCAL_IP 127.0.0.1
